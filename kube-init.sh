bd6e2b758d287a5ce2b8a459e6f24cb8298ff5f1
Marco Aurelio Miranda <ma@snoke.de>
2020-11-22 11:30:56 +0000

    improvements to kube-init

--- kube-init.sh
+++ kube-init.sh
@@ -3,12 +3,13 @@
 VERSION=$1
 
 sudo sed -i "/$(hostname)/s/^.*$/$(hostname -I|awk '{print $1}') $(hostname)/" /etc/hosts
-sudo kubeadm init --pod-network-cidr=10.168.0.0/16 --control-plane-endpoint=$(hostname) --upload-certs --kubernetes-version="stable-$VERSION"
-
-mkdir -p $HOME/.kube
-sudo cp -a /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#kubectl delete -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-#kubectl delete -f https://docs.projectcalico.org/manifests/custom-resources.yaml
-kubectl create -f https://docs.projectcalico.org/manifests/calico.yaml 
+sudo kubeadm init --pod-network-cidr=10.168.0.0/16 --control-plane-endpoint=$(hostname) --upload-certs --kubernetes-version="stable-$VERSION" --ignore-preflight-errors=NumCPU
+if [ $? -eq 0 ]
+then
+	mkdir -p $HOME/.kube
+	sudo cp -a /etc/kubernetes/admin.conf $HOME/.kube/config
+	sudo chown $(id -u):$(id -g) $HOME/.kube/config
+	kubectl wait --for=condition=Ready node/$(hostname) --timeout=90s
+	kubectl create -f https://docs.projectcalico.org/manifests/calico.yaml 
+	kubectl wait --for=condition=Ready node/$(hostname) --timeout=90s
+fi
